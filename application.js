38b5090bad0e25f40e9e395660485c8595f9c600
jartigag <jartigag@protonmail.com>
2018-03-24 19:50:15 +0100

    add duration, sentences

--- application.js
+++ application.js
@@ -1,13 +1,10 @@
 /* 
 
 TODO:
-- poder fijar al principio la duración del ejercicio
-
 - penalización por repetir palabras (x2 si es en la misma frase)
 
-	pensar en el diseño de la notificación de penalización para que el usuario se dé cuenta pero no pierda el flujo de escritura
-
-- imprimir resultados al final: tiempo, kpm, wpm, frases, penalización
+	pensar en el diseño de la notificación de penalización
+	para que el usuario se dé cuenta pero no pierda el flujo de escritura
 
 - registrar los ejercicios (tanto los contadores como el texto) durante la sesión
 
@@ -15,82 +12,107 @@ TODO:
 
 */
 
-var beginTime,
-	started = false,
-	stroken = 0,
+var strokes = 0,
+	writtenStrokes = 0,
+	words = 0,
 	writtenWords = 0,
-	current;
+	sentences = 0,
+	beginTime,
+	duration = 60, //in seconds
+	started = false,
+	ended = false;
 
 writing.addEventListener( 'click', function(){
+
+	if( !started && !ended ){
+		started = true;
+		strokes = 0;
+		beginTime = Date.now();
+	}
 
 	if (writing.innerText=='haz click aquí para empezar a escribir') {
 		writing.contentEditable = true;
 		writing.innerText = '';
-	}
-
-	if( !started ){
-
-		started = true;
-		stroken = 0;
-		beginTime = Date.now();
+		writing.focus();
 	}
 
 });
 
 writing.addEventListener( 'keydown', function(e){
 
-	if( !started ){
-
+	if( !started && !ended ){
 		started = true;
 		beginTime = Date.now();
 	}
 
 	reset.style.display = 'block';
 	explain_intro.style.display = 'block';
-	++stroken;
 
 	if( e.keyCode === 13 ){ // return key
-
-		writtenWords += writing.innerText.match(/(\w+)/g).length;
+		sentences++;
+		writtenStrokes = strokes;
+		writtenWords = words;
+		strokes = 0;
+		words = 0;
 		written.style.display = 'block';
 		written.innerText += writing.innerText + '\r\n';
 		writing.innerText = '';
 		e.preventDefault();
-	} else if( e.keyCode === 8 ) --stroken; // delete
+	} 
+	//else if( e.keyCode === 8 ) --strokes; // delete
 });
-
-var interval = window.setInterval( function(){
-	
-	if( started ){
-
-		var span = ( Date.now() - beginTime )
-				min = span / 60000;
-
-		kpm.innerText = ( stroken / min ) | 0; // | 0: convert anything to integer
-		if (writing.innerText.trim() == '') {
-			words = writtenWords;
-		} else {
-			words = writing.innerText.match(/(\w+)/g).length + writtenWords; //regex: \w+ between one and unlimited word characters, /g greedy - don't stop after the first match
-		}
-		wpm.innerText = ( words / min ) | 0;
-		sec = ( span % 60000 ) / 1000;
-		time.innerText = ( min | 0 ) + ':' + ( sec | 0 ).toString().padStart(2, "0");
-	}
-}, 50 );
 
 reset.addEventListener( 'click', function(){
 
 	writing.innerText = '';
 	written.innerText = '';
+	kpm.innerText = '0';
+	wpm.innerText = '0';
+	time.innerText = '0:00';
 
-	started = false;
+	time.style.color = '';
 	written.style.display = 'none';
 	reset.style.display = 'none';
 	explain_intro.style.display = 'none';
-	kpm.innerText = 0;
-	wpm.innerText = 0;
-	time.innerText = '0:00';
+
+	writtenStrokes = 0;
+	writtenWords = 0;
+	strokes = 0;
+	words = 0;
+	sentences = 0;
 
 	writing.focus();
-	stroken = 0;
+	beginTime = Date.now();
+	started = true;
+	ended = false;
 });
+
+var interval = window.setInterval( function(){
+	
+	if( started && !ended ){
+		var span = ( Date.now() - beginTime ),
+			mins = span / 60000;
+
+		if (writing.innerText.trim() == '') {
+			strokes = writtenStrokes;
+			words = writtenWords;
+		} else {
+			strokes = writing.innerText.length + writtenStrokes;
+			words = writing.innerText.match(/(\w+)/g).length + writtenWords;
+			//	regex:	\w+ between one and unlimited word characters, 
+			//			/g greedy - don't stop after the first match
+		}
+		kpm.innerText = ( strokes / mins ) | 0; // | 0: convert anything to integer
+		wpm.innerText = ( words / mins ) | 0;
+
+		secs = ( span % 60000 ) / 1000;
+		time.innerText = ( mins | 0 ) + ':' + ( secs | 0 ).toString().padStart(2, "0");
+		if (Math.floor(span / 1000) >= duration) {
+			time.style.color = 'red';
+			ended = true;
+			window.alert('time = '+time.innerText+
+				'\nkpm = '+kpm.innerText+', wpm = '+wpm.innerText+
+				'\nsentences = '+sentences); //+', penalty repeated words = '
+		}
+	}
+}, 100 );
